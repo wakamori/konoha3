6a5b46e7604ec515040c0fa6a52b0ea6b6befd31
shinpei <shinpei.nakata@gmail.com>
2012-07-15 14:04:23 +0900

    tracevm update

--- package/tracevm/tracevm_glue.h
+++ package/tracevm/tracevm_glue.h
@@ -24,7 +24,48 @@
 
 #ifndef TRACEVM_GLUE_H
 #define TRACEVM_GLUE_H_
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
+#define MOD_tracevm 97 /* TODO */
+#define ktracemod ((ktracemod_t*)kctx->mod[MOD_tracevm])
+#define kmodtrace ((kmodtrace_t*)kctx->modshare[MOD_tracevm])
+#define GenCodeMtd (kmodtrace)->genCode
+#define GenCodeDefault (kmodtrace)->defaultCodeGen
 
+typedef void (*FgenCode)(KonohaContext *, kMethod *, kBlock * );
+typedef struct {
+	KonohaModule h;
+	kMethod *genCode;
+	kArray *constPool;
+	FgenCode defaultCodeGen;
+}kmodtrace_t;
+
+typedef struct {
+	KonohaContextModule h;
+} ktracemod_t;
+
+static void kmodtrace_setup(KonohaContext *kctx, struct KonohaModule *def, int newctx)
+{
+	(void)kctx;
+	(void)def;
+	(void)newctx;
+}
+extern kObjectVar **KONOHA_reftail(KonohaContext *kctx, size_t size);
+
+static void kmodtrace_reftrace(KonohaContext *kctx, struct KonohaModule*baseh)
+{
+	kmodtrace_t *mod = (kmodtrace_t *)baseh;
+	BEGIN_REFTRACE(1);
+	KREFTRACEv(mod->genCode);
+	END_REFTRACE();
+}
+
+static void kmodtrace_free(KonohaContext *kctx, struct KonohaModule *baseh)
+{
+	kmodtrace_t *modshare = (kmodtrace_t *)baseh;
+	KFREE(baseh, sizeof(kmodtrace_t));
+
+}
 /* ------------------------------------------------------------------------ */
 
 #define _Public   kMethod_Public
@@ -38,26 +79,38 @@
 static void kGenTraceCode(KonohaContext *kctx, kMethod *mtd, kBlock *bk)
 {
 	DBG_P("start trace code generation");
-
+	BEGIN_LOCAL(lsfp, 8);
+	KSETv(lsfp[K_CALLDELTA+1].asMethod, mtd);
+	KSETv(lsfp[K_CALLDELTA+2].asObject, (kObject*)bk);
+	KCALL(lsfp, 0, GenCodeMtd, 2, K_NULL);
+	END_LOCAL();
 }
 
 static kbool_t tracevm_initPackage(KonohaContext *kctx, kNameSpace *ns, int argc, const char **args, kfileline_t pline)
 {
+	kmodtrace_t *base = (kmodtrace_t*)KCALLOC(sizeof(kmodtrace_t), 1);
+	base->h.name         = "tracevm";
+	base->h.setup        = kmodtrace_setup;
+	base->h.reftrace     = kmodtrace_reftrace;
+	base->h.free         = kmodtrace_free;
+	base->defaultCodeGen = kctx->klib->kMethod_genCode;
+
+	KLIB Konoha_setModule(kctx, MOD_tracevm, &base->h, pline);
 	return true;
 }
 
 static kbool_t tracevm_setupPackage(KonohaContext *kctx, kNameSpace *ns, kfileline_t pline)
 {
-
-	LibKonohaApiVar *l = (LibKonohaApiVar*)kctx->klib;
-	l->kMethod_genCode = GenTraceCode;
-	kNameSpace_syncMethods();
+	kMethod *mtd = KLIB kNameSpace_getMethodNULL(kctx, ns, TY_System, MN_("genCode"));
+	KonohaLibVar *l = (KonohaLibVar*)kctx->klib;
+	l->kMethod_genCode = GenCodeDefault;
+	KLIB kNameSpace_syncMethods(kctx);
 	return true;
 }
 
 static kbool_t tracevm_initNameSpace(KonohaContext *kctx, kNameSpace *ns, kfileline_t pline)
 {
-	USING_SUGAR;
+
 	return true;
 }
 
