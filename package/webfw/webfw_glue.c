055c642bb46870141170ccd68f498e8be007ec56
wakamori <wakamori111@gmail.com>
2012-07-22 20:21:48 +0900

    update interface

--- package/webfw/webfw_glue.c
+++ package/webfw/webfw_glue.c
@@ -22,9 +22,9 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ***************************************************************************/
 
-#include<minikonoha/minikonoha.h>
-#include<minikonoha/sugar.h>
-#include"webfw_glue.h"
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
+#include "webfw_glue.h"
 
 // --------------------------------------------------------------------------
 
