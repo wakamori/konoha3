0d842e7f2ff32a025123fe8b2955afdcf079d868
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-01-23 19:12:19 +0900

    add Exception_glue.c

---
+++ src/package-devel/MiniKonoha.Exception/Exception_glue.c
@@ -0,0 +1,81 @@
+/****************************************************************************
+ * Copyright (c) 2012, the Konoha project authors. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+#include <curl/curl.h>
+
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
+#include <minikonoha/import/methoddecl.h>
+
+struct JsonBuf;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+static kbool_t ThrowExceptionToEventListener(KonohaContext *kctx, struct JsonBuf *info)
+{
+	if(info == NULL || PLATAPI IsJsonType(info, KJSON_NULL)) {
+		return false;
+	}
+	const char *text = PLATAPI JsonToNewText(kctx, info);
+	CURL *curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8080");
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, text);
+	CURLcode res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+	switch(res) {
+		case CURLE_OK:
+			return true;
+		default:
+			return false;
+	}
+}
+
+static kbool_t Exception_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int option, KTraceInfo *trace)
+{
+	KonohaFactory *factory = (KonohaFactory *)kctx->platApi;
+	factory->ThrowExceptionToEventListener = ThrowExceptionToEventListener;
+	PLATAPI ThrowExceptionToEventListener(kctx, NULL);
+	return true;
+}
+
+static kbool_t Exception_ExportNameSpace(KonohaContext *kctx, kNameSpace *ns, kNameSpace *exportNS, int option, KTraceInfo *trace)
+{
+	return true;
+}
+
+KDEFINE_PACKAGE *Exception_Init(void)
+{
+	static KDEFINE_PACKAGE d = {
+		KPACKNAME("exception", "1.0"),
+		.PackupNameSpace    = Exception_PackupNameSpace,
+		.ExportNameSpace   = Exception_ExportNameSpace,
+	};
+	return &d;
+}
+
+#ifdef __cplusplus
+}
+#endif
